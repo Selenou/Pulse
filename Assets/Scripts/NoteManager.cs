ab0f243ef883c953664d303c5c2c7be5f43b14d2
Romain Richard <romain.p.richard@usherbrooke.ca>
2018-05-21 15:36:37 +0200

    ajout Note manager

---
+++ Assets/Scripts/NoteManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour {
+
+	public GameObject note;
+
+	void Start () {
+		Instantiate(note, new Vector3(0f, 0.05f, 23.7f), Quaternion.identity, GameObject.Find("Notes").transform);
+	}
+}
